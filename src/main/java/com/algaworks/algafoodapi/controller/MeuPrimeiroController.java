22096756827d4a312f390a7e3abfc360d79d281c
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2024-01-09 03:48:27 -0300

    Injetando dependências (beans Spring)

--- src/main/java/com/algaworks/algafoodapi/controller/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafoodapi/controller/MeuPrimeiroController.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafoodapi.controller;
 
+import com.algaworks.algafoodapi.modelo.Cliente;
+import com.algaworks.algafoodapi.service.AtivacaoClienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +9,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MeuPrimeiroController {
 
+    private AtivacaoClienteService service;
+
+    public MeuPrimeiroController(AtivacaoClienteService service) {
+        this.service = service;
+    }
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
+        Cliente joao = new Cliente("João", "joao@xyz.com", "39462859");
+
+        service.ativar(joao);
+
         return "Hello";
     }
 
--- src/main/java/com/algaworks/algafoodapi/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafoodapi/service/AtivacaoClienteService.java
@@ -1,13 +1,17 @@
 package com.algaworks.algafoodapi.service;
 
 import com.algaworks.algafoodapi.modelo.Cliente;
-import com.algaworks.algafoodapi.notificacao.NotificadorEmail;
+import com.algaworks.algafoodapi.notificacao.Notificador;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
-    private NotificadorEmail notificador;
+    private Notificador notificador;
+
+    public AtivacaoClienteService(Notificador notificador) {
+        this.notificador = notificador;
+    }
 
     public void ativar(Cliente cliente){
         cliente.ativar();
