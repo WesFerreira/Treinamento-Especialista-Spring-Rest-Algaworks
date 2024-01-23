7fa6d84fe474932bf2f33a8d83e0a5c2ab6b1867
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2024-01-22 22:25:27 -0300

    2.18. Desambiguação de beans com @Qualifier

--- src/main/java/com/algaworks/algafoodapi/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafoodapi/notificacao/NotificadorEmail.java
@@ -1,8 +1,11 @@
 package com.algaworks.algafoodapi.notificacao;
 
 import com.algaworks.algafoodapi.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Qualifier("email")
 @Component
 public class NotificadorEmail implements Notificador {
 
--- src/main/java/com/algaworks/algafoodapi/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafoodapi/service/AtivacaoClienteService.java
@@ -3,6 +3,7 @@ package com.algaworks.algafoodapi.service;
 import com.algaworks.algafoodapi.modelo.Cliente;
 import com.algaworks.algafoodapi.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,15 +11,15 @@ import java.util.List;
 @Component
 public class AtivacaoClienteService {
 
+    @Qualifier("email")
     @Autowired
-    private List<Notificador> notificadores;
+    private Notificador notificador;
 
     public void ativar(Cliente cliente){
         cliente.ativar();
 
-        for (Notificador notificador : notificadores) {
             notificador.notificar(cliente, "Seu cadastro no sistema esstá ativo!");
-        }
+
     }
 
 }
