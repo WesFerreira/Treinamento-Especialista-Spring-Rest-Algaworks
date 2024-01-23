62295640e77c28760d220dd764e6b76ba1fe1217
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2024-01-22 22:09:16 -0300

    2.16-ambiguidade de beans, resolvendo com List de beans

--- src/main/java/com/algaworks/algafoodapi/notificacao/NotificadorSMS.java
+++ src/main/java/com/algaworks/algafoodapi/notificacao/NotificadorSMS.java
@@ -4,11 +4,12 @@ import com.algaworks.algafoodapi.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorSMS implements Notificador {
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificando %s através do email %s usando SMTP: %s\n",
-                cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.printf("Notificando %s por SMS através do telefone %s: %s\n",
+                cliente.getNome(), cliente.getTelefone(), mensagem);
     }
+
 }
--- src/main/java/com/algaworks/algafoodapi/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafoodapi/service/AtivacaoClienteService.java
@@ -2,20 +2,23 @@ package com.algaworks.algafoodapi.service;
 
 import com.algaworks.algafoodapi.modelo.Cliente;
 import com.algaworks.algafoodapi.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
 public class AtivacaoClienteService {
 
-    private Notificador notificador;
-
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-    }
+    @Autowired
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente){
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro no sistema esstá ativo!");
+        for (Notificador notificador : notificadores) {
+            notificador.notificar(cliente, "Seu cadastro no sistema esstá ativo!");
+        }
     }
 
 }
